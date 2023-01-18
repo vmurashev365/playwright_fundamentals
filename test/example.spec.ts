ff822bb05e796fc84b5711008d63f390452c6c76
Victor Murashev <vmurashev@gmail.com>
2023-01-18 17:12:47 +0200

    Config2

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.skip("Simple basic test", async ({ page }) => {
+test.only("Simple basic test", async ({ page }) => {
     await page.goto('https://www.example.com')
     const pageTitle = await page.locator('h1')
     await expect(pageTitle).toContainText('Example Domain')
