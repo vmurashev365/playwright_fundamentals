95c4320126b9717cc856ea4c7e6b54bc29758ecf
Victor Murashev <vmurashev@gmail.com>
2023-01-20 22:34:19 +0200

    pom_home_page

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -12,7 +12,6 @@ export class LoginPage{
 //Init selectors using constructor
 constructor (page: Page) {
     this.page = page
-    this.signinButton = page.locator('#signin_button')
     this.usernameInput = page.locator('#user_login')
     this.passwordInput = page.locator('#user_password')
     this.submitButton = page.locator("input[name='submit']")
@@ -20,12 +19,8 @@ constructor (page: Page) {
 }
 
 //Define login page methods
-    async visit() {
-        await this.page.goto('http://zero.webappsecurity.com')
-    }
 
     async login(username: string, password: string) {
-        await this.signinButton.click()
         await this.usernameInput.type(username)
         await this.passwordInput.type(password)
         await this.submitButton.click()
