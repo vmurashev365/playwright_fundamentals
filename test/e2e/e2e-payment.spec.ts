c21ef1c2d59035f6472e5b65a776f6cf8663830f
Victor Murashev <vmurashev@gmail.com>
2023-01-22 15:47:01 +0200

    remove .only

--- test/e2e/e2e-payment.spec.ts
+++ test/e2e/e2e-payment.spec.ts
@@ -6,7 +6,7 @@ import { HomePage } from '../../page-objects/HomePage'
 import { PaymentPage } from '../../page-objects/PaymentPage'
 import { Navbar } from '../../page-objects/components/Navbar'
 
-test.describe.only("New payment", () => {
+test.describe("New payment", () => {
     let homePage: HomePage
     let loginPage: LoginPage
     let paymentPage: PaymentPage
